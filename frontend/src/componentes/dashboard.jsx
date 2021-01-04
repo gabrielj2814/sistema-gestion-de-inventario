1a416255ecc867261c4af3c5249dac8aaf0acb79
Gabriel Valera <40225548+gabrielj2814@users.noreply.github.com>
2021-01-03 23:40:44 -0400

    agregando menu tooltip al navbar

--- frontend/src/componentes/dashboard.jsx
+++ frontend/src/componentes/dashboard.jsx
@@ -10,18 +10,26 @@ class Dashboard extends React.Component {
 
     constructor(){
         super()
+        this.menuBar=this.menuBar.bind(this)
         this.state={
+
+            estadoMenuBar:false
 
         }
 
+    }
+
+    menuBar(){
+        this.setState({estadoMenuBar:!this.state.estadoMenuBar})
+        // alert("hola")
     }
 
     render(){
         return(
 
             <div className="componenteDashboard">
-                <BarraSuperiorSitema/>
-                <MenuDashBoard/>
+                <BarraSuperiorSitema eventoMenuBar={this.menuBar}/>
+                <MenuDashBoard estadoMenuBar={this.state.estadoMenuBar}/>
             </div>
 
         )
